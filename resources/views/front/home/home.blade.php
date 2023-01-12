f6b248f995b42075f87ca8e934beac8431e188c7
sajeeb-barai <sajeebbarai8290@gmail.com>
2023-01-12 17:15:51 +0600

    all file need to upload

---
+++ resources/views/front/home/home.blade.php
@@ -0,0 +1,1 @@
+@extends('master.front.master')
---
+++ resources/views/master/front/master.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link type="text/css" rel="stylesheet" href="{{asset('/')}}css/all.css">
+    <link type="text/css" rel="stylesheet" href="{{asset('/')}}css/bootstrap.css">
+    <link type="text/css" rel="stylesheet" href="{{asset('/')}}css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-md bg-light ">
+        <div class="container">
+            <a href="" class="navbar-brand">LOGO</a>
+            <ul class="navbar-nav">
+                <li><a href="" class="nav-link">Home</a></li>
+                <li><a href="" class="nav-link">All Course</a></li>
+                <li><a href="" class="nav-link">Login</a></li>
+                <li><a href="" class="nav-link">Registration</a></li>
+
+            </ul>
+
+        </div>
+    </nav>
+
+    <script src="{{asset('/')}}js/jquery.min.js"></script>
+    <script src="{{asset('/')}}js/bootstrap.js"></script>
+</body>
+</html>
